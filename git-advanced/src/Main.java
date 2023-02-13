cc22e9552f1b4e1b0cd0b21309ed6913284d0634
Fanni Szvoboda <fanni.szvoboda@gmail.com>
2023-02-13 13:50:51 +0100

    b4

--- git-advanced/src/Main.java
+++ git-advanced/src/Main.java
@@ -4,16 +4,11 @@ public class Main {
         System.out.println("Taurus - " + who);
     }
     public static void main(String[] args) {
-        greet();
-
     }
-<<<<<<< HEAD
+
     public void greet(){
         System.out.println("Greetings!");
-=======
-    public static void greet(){
-        System.out.println("Erika");
->>>>>>> 9bebe45a795ba7da673a38795a519241c5a7b784
+
     }
 
     public void cheer() {
