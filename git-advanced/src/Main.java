8beebe976e18e590deabfd28c47bfc260b9376b3
Nikola Jaholcikova <nika.jaholcikova023@gmail.com>
2023-02-13 13:25:29 +0100

    c1

--- git-advanced/src/Main.java
+++ git-advanced/src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public void cheer() {
+        System.out.println("Barna's Tribes");
+    }
 }
